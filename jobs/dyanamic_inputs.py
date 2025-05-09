67e1b6d679869f956dbf7713a407c0814836c661
alhogan <98360253+alhogan@users.noreply.github.com>
2025-05-08 20:34:17 -0500

    added dynamic form job

---
+++ jobs/dyanamic_inputs.py
@@ -0,0 +1,26 @@
+from nautobot.apps.jobs import Job, StringVar, ChoiceVar, register_jobs
+
+CHOICES = (
+    ("-", "----"),
+    ("n", "North"),
+    ("s", "South"),
+)
+
+
+class Example(Job):
+    template_name = "./my_template.html"
+    var1 = ChoiceVar(choices=CHOICES, default="-")
+    var2 = StringVar()
+    var3 = StringVar()
+
+    class Meta:
+        name = "Example Job"
+
+    def run(self):
+        """Executes the Job."""
+        pass
+
+
+jobs = [Example]
+
+register_jobs(*jobs)
